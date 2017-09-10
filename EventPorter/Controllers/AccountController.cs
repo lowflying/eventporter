2fa565bb0ca885040e219ce4b9594f6397e9e7c8
lowflying <lowflyingnow@gmail.com>
2017-09-10 17:41:19 +0100

    more work on the profile, started ajax stuff

--- EventPorter/Controllers/AccountController.cs
+++ EventPorter/Controllers/AccountController.cs
@@ -69,7 +69,7 @@ namespace EventPorter.Controllers
             return View("../Home/Index");
         }
 
-        public ActionResult Profile()
+        public ActionResult AdamInfo()
         {
             return View();
         }
@@ -86,6 +86,12 @@ namespace EventPorter.Controllers
             return View();
 
             
+        }
+
+        public ActionResult Settings()
+        {
+
+            return View();
         }
 
     }
